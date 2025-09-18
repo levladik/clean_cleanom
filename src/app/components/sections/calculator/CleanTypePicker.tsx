51da844a29d2e614c8af5c8341242251208ff397
Levladik <vladislavtyshenko@yandex.ru>
2025-09-18 16:13:59 +0400

    feat(ui): add tooltips to clean type picker tabs

--- src/app/components/sections/calculator/CleanTypePicker.tsx
+++ src/app/components/sections/calculator/CleanTypePicker.tsx
@@ -9,23 +9,44 @@
 import { useState } from 'react'
 
 const CleanTypePicker = () => {
-  const tabs = ['Base', 'Deep', 'Rennovation']
+  const tabs = [
+    {
+      name: 'Base',
+      tooltip:
+        'Cухая и влажная уборка помещений (удаление пыли и загрязнений с открытых поверхностей), уборка санузлов и кухни (открытые поверхности), замена постельного белья, вынос мусора. ',
+    },
+    {
+      name: 'Deep',
+      tooltip:
+        'Cухая и влажная уборка помещений (включая труднодоступные места, светильники и внутренние поверхности мебели), уборка санузлов и кухни (включая удаление сложных загрязнений, очистку труднодоступных мест и внутренних отделений шкафчиков), удаление паутины и плесени, мытье окон, замена постельного белья, вынос мусора.',
+    },
+    {
+      name: 'Rennovation',
+      tooltip:
+        'Очистка поверхностей от строительной пыли и побелки, сухая и влажная уборка помещений (включая труднодоступные места и внутренние отделения мебели), уборка санузлов и кухни (включая сложные загрязнения), мытье окон (включая удаление следов клея и краски), вынос некрупногабаритного мусора. ',
+    },
+  ]
   const [activeTab, setActiveTab] = useState('Deep')
 
   return (
     <div className="join">
       {/* Tabs */}
       {tabs.map((tab, idx) => (
-        <input
+        <div
           key={idx}
-          aria-label={tab}
-          checked={activeTab === tab}
-          className="join-item btn btn-xs sm:btn-sm"
-          name={tab}
-          onChange={(e) => setActiveTab(e.target.value)}
-          type="radio"
-          value={tab}
-        />
+          className="tooltip tooltip-bottom"
+          data-tip={tab.tooltip}
+        >
+          <input
+            aria-label={tab.name}
+            checked={activeTab === tab.name}
+            className="join-item btn btn-xs sm:btn-sm"
+            name={tab.name}
+            onChange={(e) => setActiveTab(e.target.value)}
+            type="radio"
+            value={tab.name}
+          />
+        </div>
       ))}
     </div>
   )
